53c52d0d4a974daf0864a61ca3d4620a6db34160
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-05-01 17:23:48 +0900

    Add browser launcher

---
+++ constants.js
@@ -0,0 +1,14 @@
+module.exports = {
+  scraper: {
+    apiUrl: 'https://board.sejong.ac.kr/boardlist.do',
+    topics: {
+      333: 'General Notice (공지)',
+      334: 'Admission (입학)',
+      335: 'Bachelor (학사)',
+      674: 'International Exchange (국제교류)',
+      337: 'Employment (취업)',
+      338: 'Scholarship (장학)',
+      339: 'School Recruitment (교네모집)',
+    },
+  },
+};
---
+++ scraper/browser.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+
+const startBrowser = async () => {
+  let browser;
+
+  try {
+    browser = await puppeteer.launch({
+      headless: false,
+      args: ['--disable-setuid-sandbox'],
+      ignoreHTTPSErrors: true,
+    });
+  } catch (err) {
+    console.log('Could not create a browser instance => : ', err);
+  }
+
+  return browser;
+};
+
+module.exports = {
+  startBrowser,
+};
