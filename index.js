ff574fa3f69ec16b02f2bfa10bbbdb214c57a7d6
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-05-02 00:47:27 +0900

    Remove unused code

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ const crawlAnnouncement = async () => {
     await Promise.all(result.map(async (announcement) => {
       const saved = await announcementModel.getAnnouncementById(announcement.id);
       if (!saved) {
-        console.log(announcement);
         await announcementModel.addAnnouncement(
           announcement.id,
           announcement.title,
@@ -61,43 +60,6 @@ const crawlAnnouncement = async () => {
 // });
 
 const test = async () => {
-  // const users = await userModel.getAllSubscribedUsers();
-  // console.log(users);
-  // await Promise.all(users.map(async (user) => {
-
-  //   const additionalMessage = 'There was a problem with the translation.\n';
-  //   bot.sendMessage(
-  //     user.id,
-  //     `${additionalMessage}Original Announcement: <a href='https://google.com'>Tap here</a>`,
-  //     { parse_mode: 'HTML' },
-  //   );
-  // }));
-
-  // const result = await worker.scrap(333);
-  // await Promise.all(result.map(async (announcement) => {
-  //   const saved = await announcementModel.getAnnouncementById(announcement.id);
-  //   if (!saved) {
-  //     await announcementModel.addAnnouncement(
-  //       announcement.id,
-  //       announcement.title,
-  //       announcement.link,
-  //       announcement.date,
-  //       announcement.writer,
-  //     );
-
-  //     const translatedText = await translator.translate(announcement.content);
-  //     const users = await userModel.getAllSubscribedUsers();
-
-  //     await Promise.all(users.map(async (user) => {
-  //       await bot.sendMessage(user.id, translatedText);
-  //       bot.sendMessage(
-  //         user.id,
-  //         `Original Announcement: <a href='${announcement.link}'>Tap here</a>`,
-  //         { parse_mode: 'HTML' },
-  //       );
-  //     }));
-  //   }
-  // }));
   await crawlAnnouncement();
 };
 
