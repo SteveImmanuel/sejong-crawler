a220badb43258c6b1baada6a1e5412fdf4e4adbf
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-05-02 10:27:57 +0900

    Handle long messages

--- index.js
+++ index.js
@@ -13,37 +13,51 @@ const crawlAnnouncement = async () => {
   for (let i = 0; i < topics.length; i += 1) {
     const [key, value] = topics[i];
     console.log(`Scraping announcement of ${value}`);
+
     // eslint-disable-next-line no-await-in-loop
     const result = await worker.scrap(key);
     // eslint-disable-next-line no-await-in-loop
     await Promise.all(result.map(async (announcement) => {
       const saved = await announcementModel.getAnnouncementById(announcement.id);
       if (!saved) {
-        await announcementModel.addAnnouncement(
-          announcement.id,
-          announcement.title,
-          announcement.link,
-          announcement.date,
-          announcement.writer,
-        );
+        console.log(`New announcement found, id: ${announcement.id}`);
+        try {
+          await announcementModel.addAnnouncement(
+            announcement.id,
+            announcement.title,
+            announcement.link,
+            announcement.date,
+            announcement.writer,
+          );
+        } catch (err) {
+          console.error(err);
+        }
 
         console.log(`Translating announcement ${announcement.id}`);
-        const [translatedText, success] = await translator.translate(announcement.content);
+        const [[transText, trans1Success], [transTitle, trans2Success]] = await Promise.all([
+          translator.translate(announcement.content),
+          translator.translate(announcement.title),
+        ]);
         const users = await userModel.getAllSubscribedUsers();
 
         console.log(`Sending announcement ${announcement.id} to subscribed users`);
         await Promise.all(users.map(async (user) => {
           try {
-            await bot.sendMessage(user.id, translatedText);
+            let announcementContent = transText;
+            if (transText.length > constant.bot.telegram.maxLength) {
+              announcementContent = 'Announcement is too long to be sent 😥, please check the announcement link.';
+            }
+
             let additionalMessage = '';
-            if (!success) {
+            if (!trans1Success || !trans2Success) {
               additionalMessage = 'There was a problem with the translation.\n';
             }
-            bot.sendMessage(
+            await bot.sendMessage(
               user.id,
-              `${additionalMessage}Original Announcement: <a href='${announcement.link}'>Tap here</a>`,
+              `Title: <b>${transTitle}</b>\nOriginal Announcement: <a href='${announcement.link}'>Tap here</a>\n${additionalMessage}`,
               { parse_mode: 'HTML' },
             );
+            await bot.sendMessage(user.id, announcementContent);
           } catch (error) {
             console.error(error);
           }
@@ -55,12 +69,7 @@ const crawlAnnouncement = async () => {
   console.timeEnd('Total Time of Crawling Announcement');
 };
 
-// cron.schedule('*/20 * * * * *', async () => {
-//   await crawlAnnouncement();
-// });
-
-const test = async () => {
+// schedule task to run every 30 minutes
+cron.schedule('*/30 * * * *', async () => {
   await crawlAnnouncement();
-};
-
-test();
+});
