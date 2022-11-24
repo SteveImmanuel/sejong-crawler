a13aeac4c4ad11f9574c855eab5ebd8ac8ec0890
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-11-24 11:08:02 +0900

    Implement unlimited translate

--- utils/translator.js
+++ utils/translator.js
@@ -1,5 +1,9 @@
 const axios = require('axios');
 const constant = require('../constants');
+const { startBrowser } = require('../scraper/browser');
+
+const fixNewline = (text) => text.replace(/\n\n/g, '\n').replace(/\n{3,}/g, '\n\n');
+const convertToUrlEncoding = (text) => text.replace(/\n/g, '%0A').replace(/\s/g, '%20'); // space and newline
 
 const postRequest = async (url, body, config = {}) => axios.post(
   url,
@@ -32,4 +36,38 @@ const translate = async (text, sourceLang = 'ko', targetLang = 'en') => {
   }
 };
 
-module.exports = { translate };
+const translateUnl = async (text, sourceLang = 'ko', targetLang = 'en', timeout = 2000) => {
+  let browser;
+  let result;
+
+  try {
+    browser = await startBrowser();
+  } catch (error) {
+    console.error('Failed launching browser');
+    return false;
+  }
+
+  try {
+    const browserPage = (await browser.pages())[0];
+    const formattedText = convertToUrlEncoding(fixNewline(text));
+    await browserPage.goto(`https://papago.naver.com/?sk=${sourceLang}&tk=${targetLang}&st=${formattedText}`);
+    await browserPage.waitForTimeout(timeout);
+    result = await browserPage.waitForSelector('#txtTarget');
+    result = await result.evaluate((el) => {
+      let translated = el.innerHTML;
+      translated = translated.replace(/<br>/g, '\n').replace(/<[^>]*>/g, '');
+      return translated;
+    });
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await browser.close();
+  }
+  console.log(result);
+  return result;
+};
+
+// const text = '1. 할인권 구매 대상\n\n가. 신입생 및 재학생 : 당 학기 등록금을 완납한 신입생 및 재학생\n\n;
+// translateUnl(text);
+
+module.exports = { translate, translateUnl };
