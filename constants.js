a0077d19a29b96793a77623e5200be7cbafeeda3
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-11-24 17:38:50 +0900

    Update api url

--- constants.js
+++ constants.js
@@ -1,14 +1,22 @@
 module.exports = {
   scraper: {
-    apiUrl: 'https://board.sejong.ac.kr/boardlist.do',
-    topics: {
-      333: 'General Notice (공지)',
-      334: 'Admission (입학)',
-      335: 'Bachelor (학사)',
-      674: 'International Exchange (국제교류)',
-      337: 'Employment (취업)',
-      338: 'Scholarship (장학)',
-      339: 'School Recruitment (교네모집)',
+    general: {
+      apiUrl: 'https://board.sejong.ac.kr/boardlist.do',
+      topics: {
+        333: 'General Notice (공지)',
+        334: 'Admission (입학)',
+        335: 'Bachelor (학사)',
+        674: 'International Exchange (국제교류)',
+        337: 'Employment (취업)',
+        338: 'Scholarship (장학)',
+        339: 'School Recruitment (교네모집)',
+      },
+    },
+    graduate: {
+      apiUrl: {
+        information: 'https://graduate.sejong.ac.kr/graduate/information/notice.do',
+        others: 'https://graduate.sejong.ac.kr/graduate/information/etc-notice.do',
+      },
     },
   },
   translator: {
