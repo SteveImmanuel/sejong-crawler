6511edd86c64b42755d19b81b664bd5fae12207e
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-11-25 12:59:34 +0900

    Add subscriptions schema

---
+++ models/subscriptions.js
@@ -0,0 +1,47 @@
+const dbInstance = require('../db');
+
+class Subscriptions {
+  constructor() {
+    this.dbInstance = dbInstance;
+    this.tableName = 'subscriptions';
+    this.createTable();
+  }
+
+  async createTable() {
+    return this.dbInstance.run(`CREATE TABLE IF NOT EXISTS ${this.tableName} (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      userId INTEGER,
+      topicId INTEGER,
+      FOREIGN KEY (userId) REFERENCES users(id),
+      UNIQUE (userId,topicId))`);
+  }
+
+  async addSubscription(userId, topicId) {
+    return this.dbInstance.run(
+      `INSERT INTO ${this.tableName} (userId, topicId) VALUES (?, ?)`,
+      [userId, topicId],
+    );
+  }
+
+  async getSubscriptionsByTopicId(topicId) {
+    return this.dbInstance.getAll(`SELECT * FROM ${this.tableName} WHERE topicId = ?`, [topicId]);
+  }
+
+  async getSubscriptionsByUserId(userId) {
+    return this.dbInstance.getAll(`SELECT topicId FROM ${this.tableName} WHERE userId = ?`, [userId]);
+  }
+
+  async deleteUserSubscriptionsByTopicId(userId, topicId) {
+    return this.dbInstance.run(
+      `DELETE FROM ${this.tableName} WHERE userId = ? AND topicId = ?`,
+      [userId, topicId],
+    );
+  }
+
+  async deleteAllUserSubscriptions(userId) {
+    return this.dbInstance.run(`DELETE FROM ${this.tableName} WHERE userId = ?`, [userId]);
+  }
+}
+
+const subscriptionModel = new Subscriptions();
+module.exports = subscriptionModel;
