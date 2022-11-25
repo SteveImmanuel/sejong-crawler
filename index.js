136f8f83ea671fcfd801fce9eb4cf2ec56e81976
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-11-25 18:10:46 +0900

    Await async sequentially

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
 const cron = require('node-cron');
 const worker = require('./scraper/worker');
 const bot = require('./bot/telegram');
 const announcementModel = require('./models/announcements');
-const { topics } = require('./utils/topics');
+const { topics, idToTopic } = require('./utils/topics');
 const userModel = require('./models/users');
 const translator = require('./utils/translator');
 const constant = require('./constants');
 const { startBrowser } = require('./scraper/browser');
 
-const translateAndBroadcastAnnouncement = async (browser, announcement, topicId) => {
-  console.log(`Translating announcement ${announcement.id}`);
-
+const translateAnnouncement = async (browser, announcement, topicId) => {
   const users = await userModel.getUserByTopicId(topicId);
   const uniqueLangs = Array.from(new Set(users.map((user) => user.lang)));
   const translated = new Map();
@@ -26,6 +26,45 @@ const translateAndBroadcastAnnouncement = async (browser, announcement, topicId)
       success: trans1Success && trans2Success,
     });
   }));
+  return translated;
+};
+
+const translateSequentialAnnouncement = async (browser, announcement, topicId) => {
+  const users = await userModel.getUserByTopicId(topicId);
+  const uniqueLangs = Array.from(new Set(users.map((user) => user.lang)));
+  const translated = new Map();
+
+  for (const lang of uniqueLangs) {
+    const [transText, trans1Success] = await translator.translateUnl(
+      browser,
+      announcement.content,
+      lang,
+    );
+    const [transTitle, trans2Success] = await translator.translateUnl(
+      browser,
+      announcement.title,
+      lang,
+    );
+    translated.set(lang, {
+      title: transTitle,
+      text: transText,
+      success: trans1Success && trans2Success,
+    });
+  }
+
+  return translated;
+};
+
+const translateAndBroadcastAnnouncement = async (browser, announcement, topicId, async = true) => {
+  console.log(`Translating announcement ${announcement.id}`);
+  const users = await userModel.getUserByTopicId(topicId);
+  let translated;
+
+  if (async) {
+    translated = await translateAnnouncement(browser, announcement, topicId);
+  } else {
+    translated = await translateSequentialAnnouncement(browser, announcement, topicId);
+  }
 
   console.log(`Sending announcement ${announcement.id} to subscribed users`);
   await Promise.all(users.map(async (user) => {
@@ -43,7 +82,7 @@ const translateAndBroadcastAnnouncement = async (browser, announcement, topicId)
 
       await bot.sendMessage(
         user.id,
-        `<b>${annTitle}</b>\n<a href='${announcement.link}'>Original Link</a>\n${additionalMessage}\n${annContent}`,
+        `<b>${annTitle}</b>\n<b>Topic: </b>${idToTopic.get(topicId)}\n<a href='${announcement.link}'>Original Link</a>\n${additionalMessage}\n${annContent}`,
         { parse_mode: 'HTML' },
       );
     } catch (error) {
@@ -52,6 +91,8 @@ const translateAndBroadcastAnnouncement = async (browser, announcement, topicId)
   }));
 };
 
+// requires very big RAM on first launch
+// eslint-disable-next-line no-unused-vars
 const crawlAnnouncement = async () => {
   let translatorBrowser;
   try {
@@ -66,7 +107,7 @@ const crawlAnnouncement = async () => {
     const announcementIds = await announcementModel.getAllAnnoucementIds();
     const scrappedIds = new Set(announcementIds.map((ann) => ann.announcementId));
     await Promise.all(topics.map(async ([id, topic]) => {
-      console.log(`Scraping undergraduate announcement of ${topic}`);
+      console.time(`Scraping announcement of ${topic}`);
 
       let result;
       if (id > 900) {
@@ -81,7 +122,7 @@ const crawlAnnouncement = async () => {
       }
 
       await Promise.all(result.map(async (announcement) => {
-        console.log(`New announcement found, id: ${announcement.id}`);
+        console.log(`New announcement found, id: ${announcement.id}, topic: ${topic}`);
         try {
           await announcementModel.addAnnouncement(
             announcement.id,
@@ -95,6 +136,7 @@ const crawlAnnouncement = async () => {
 
         await translateAndBroadcastAnnouncement(translatorBrowser, announcement, id);
       }));
+      console.timeEnd(`Scraping announcement of ${topic}`);
     }));
   } catch (error) {
     console.error(error);
@@ -105,9 +147,63 @@ const crawlAnnouncement = async () => {
   console.timeEnd('Total Time of Crawling Announcement');
 };
 
-crawlAnnouncement();
+const crawlSequentialAnnouncement = async () => {
+  let translatorBrowser;
+  try {
+    translatorBrowser = await startBrowser();
+  } catch (error) {
+    console.error('Failed launching browser');
+  }
+
+  console.time('Total Time of Crawling Announcement');
+
+  try {
+    const announcementIds = await announcementModel.getAllAnnoucementIds();
+    const scrappedIds = new Set(announcementIds.map((ann) => ann.announcementId));
+    for (const [id, topic] of topics) {
+      console.time(`Scraping announcement of ${topic}`);
+
+      let result;
+      if (id > 900) {
+        result = await worker.scrap(
+          constant.scraper.graduate.topics[id].apiUrl,
+          scrappedIds,
+          true,
+          10,
+        );
+      } else {
+        result = await worker.scrap(worker.buildUgradApiUrl(id), scrappedIds, false, 10);
+      }
+
+      for (const announcement of result) {
+        console.log(`New announcement found, id: ${announcement.id}, topic: ${topic}`);
+        try {
+          await announcementModel.addAnnouncement(
+            announcement.id,
+            announcement.title,
+            announcement.link,
+            id,
+          );
+        } catch (err) {
+          console.error(err);
+        }
+        await translateAndBroadcastAnnouncement(translatorBrowser, announcement, id, false);
+      }
+
+      console.timeEnd(`Scraping announcement of ${topic}`);
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await translatorBrowser.close();
+  }
+
+  console.timeEnd('Total Time of Crawling Announcement');
+};
+
+crawlSequentialAnnouncement();
 
 // schedule task to run periodically
 cron.schedule(`*/${constant.system.crawlInterval} * * * *`, async () => {
-  await crawlAnnouncement();
+  await crawlSequentialAnnouncement();
 });
