c5aba768ff6f0821e7c3ee0009197084a2d8d5e6
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-11-25 12:59:00 +0900

    Simulate keyboard in translating long text

--- utils/translator.js
+++ utils/translator.js
@@ -1,8 +1,7 @@
 const axios = require('axios');
 const constant = require('../constants');
-const { startBrowser } = require('../scraper/browser');
 
-const fixNewline = (text) => text.replace(/\n\n/g, '\n').replace(/\n{3,}/g, '\n\n');
+const fixNewline = (text) => text.replace(/\n\n/g, '\n').replace(/\n{3,}/g, '\n\n').replace(/&nbsp;/g, ' ');
 const convertToUrlEncoding = (text) => text.replace(/\n/g, '%0A').replace(/\s/g, '%20'); // space and newline
 
 const postRequest = async (url, body, config = {}) => axios.post(
@@ -36,38 +35,58 @@ const translate = async (text, sourceLang = 'ko', targetLang = 'en') => {
   }
 };
 
-const translateUnl = async (text, targetLang = 'en', timeout = 2000) => {
-  let browser;
-  let result;
+const translateUnl = async (browser, text, targetLang = 'en', timeout = 2000) => {
+  let browserPage;
 
   try {
-    browser = await startBrowser();
+    browserPage = await browser.newPage();
   } catch (error) {
-    console.error('Failed launching browser');
-    return false;
+    console.error('Failed new tab in translator browser');
+    return [false, false];
   }
 
+  let result;
+  let success = true;
+
   try {
-    const browserPage = (await browser.pages())[0];
-    const formattedText = convertToUrlEncoding(fixNewline(text));
-    await browserPage.goto(`https://papago.naver.com/?sk=auto&tk=${targetLang}&st=${formattedText}`);
+    result = fixNewline(text);
+    await browserPage.goto(`https://papago.naver.com/?sk=auto&tk=${targetLang}`);
+
     await browserPage.waitForTimeout(timeout);
+    await browserPage.keyboard.type(result);
+    await browserPage.waitForTimeout(timeout);
+
     result = await browserPage.waitForSelector('#txtTarget');
     result = await result.evaluate((el) => {
       let translated = el.innerHTML;
-      translated = translated.replace(/<br>/g, '\n').replace(/<[^>]*>/g, '');
+      translated = translated.replace(/&nbsp;/g, ' ').replace(/<br>/g, '\n').replace(/<[^>]*>/g, '');
       return translated;
     });
+
+    await browserPage.waitForTimeout(100000);
   } catch (error) {
+    success = false;
     console.error(error);
   } finally {
-    await browser.close();
+    await browserPage.close();
   }
-  console.log(result);
-  return result;
+
+  return [result, success];
 };
 
-// const text = '1. 할인권 구매 대상\n\n가. 신입생 및 재학생 : 당 학기 등록금을 완납한 신입생 및 재학생\n\n;
-// translateUnl(text);
+// const { startBrowser } = require('../scraper/browser');
+// const worker = require('../scraper/worker');
 
-module.exports = { translate, translateUnl };
+// const test = async () => {
+//   const result = await worker.scrap(worker.buildUgradApiUrl(333), new Set(), false, 2);
+//   const browser = await startBrowser();
+//   console.log(result[0].content);
+//   const text = result[0].content;
+//   const translated = await translateUnl(browser, text, 'en', 2000);
+//   console.log(translated);
+// };
+// test();
+
+module.exports = {
+  translate, translateUnl, fixNewline, convertToUrlEncoding,
+};
